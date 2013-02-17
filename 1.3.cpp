7eb62689c6104182c80c1ae1e1e726a8a21c9ec1
moophis <moophis@github.com>
2013-02-17 21:32:13 +0800

    careercup 1.3: add another method.

--- 1.3.cpp
+++ 1.3.cpp
@@ -9,35 +9,67 @@
 
 using namespace std;
 
-bool is_perm(string s1, string s2)
+/* 
+ * First method: sort two strings then compare.
+ * Time complexity: O(nlogn).
+ */
+bool is_perm_v1(string s1, string s2)
 {
-	// check whether two strings have same length
-	if ( s1.size() != s2.size() )
-		return false;
-		
-	// first, sort two strings
-	sort(s1.begin(), s1.end());
-	sort(s2.begin(), s2.end());
-	
-	// then compare two strings afterwards
-	for (int i = 0; i < s1.size(); i++)
-		if ( s1[i] != s2[i] )
-			return false;
-	return true;
+    // check whether two strings have same length
+    if ( s1.size() != s2.size() )
+        return false;
+        
+    // first, sort two strings
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+    
+    // then compare two strings afterwards
+    for (int i = 0; i < s1.size(); i++)
+        if ( s1[i] != s2[i] )
+            return false;
+    return true;
+}
+
+/*
+ * Second method: use bitmap.
+ * Time complexity: O(n).
+ */
+bool is_perm_v2(string s1, string s2)
+{
+    static int charmap[2][256];  // bitmap
+    
+    // check whether two strings have same length
+    if ( s1.size() != s2.size() )
+        return false;
+    
+    /* first, analyze each string by counting the # of 
+     * each characters among a string.
+     */
+    for (int i = 0; i < s1.size(); i++) {
+        charmap[0][int(s1[i])]++;
+        charmap[1][int(s2[i])]++;
+    }
+    /*
+     * then, compare two bitmaps
+     */
+    for (int i = 0; i < 255; i++) 
+        if ( charmap[0][i] != charmap[1][i] )
+            return false;
+    return true;
 }
 
 int main()
 {
-	string s1, s2;
-	
-	// some test cases
-	s1 = "abcde";
-	s2 = "edcab";
-	cout << "test case 1: " << is_perm(s1, s2) << endl;
-	s1 = "abcde";
-	s2 = "abccd";
-	cout << "test case 2: " << is_perm(s1, s2) << endl;
-	
-	return 0;
+    string s1, s2;
+    
+    // some test cases
+    s1 = "abcdefg";
+    s2 = "edcabgf";
+    cout << "test case 1: " << is_perm_v2(s1, s2) << endl;
+    s1 = "abcdeadfa";
+    s2 = "abccdee3r";
+    cout << "test case 2: " << is_perm_v2(s1, s2) << endl;
+    
+    return 0;
 }
 
