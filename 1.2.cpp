6a1bccbb676bf3b9caf6b1aff95042df5750b9e2
moophis <moophis@github.com>
2013-02-17 16:31:41 +0800

    careercup 1.2

---
+++ 1.2.cpp
@@ -0,0 +1,32 @@
+/*
+ * Implement a function void reverse(char* str) in C or C++ 
+ * which reverses a null-terminated string.
+ */
+ 
+#include <iostream>
+#include <cstring>
+#include <utility>
+
+using namespace std;
+
+void reverse(char *str)
+{
+	int len = strlen(str);
+	char t;
+	
+	if (len == 0)
+		return;
+		
+	for (int i = 0; i <= len / 2; i++) 
+		swap(str[i], str[len - i - 1]); 
+}
+
+int main()
+{
+	char test[] = "hello world!!!";
+	reverse(test);
+	cout << test << endl;
+	
+	return 0;
+}
+
