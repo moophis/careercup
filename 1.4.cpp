2144d3c5ab465af16348a22361dd3825bffb6d48
moophis <moophis@github.com>
2013-02-18 17:10:04 +0800

    careercup 1.4 v2

--- 1.4.cpp
+++ 1.4.cpp
@@ -1,4 +1,4 @@
-/* 
+/**
  * Write a method to replace all spaces in a string with '%20'. 
  * You may assume that the string has sufficient space at the 
  * end of the string to hold the additional characters, and 
@@ -13,13 +13,13 @@
  */
  
 #include <iostream>
-#include <sstream>
 #include <cstring>
 
 using namespace std;
 
 /**
- * Method: basic string manipulations.
+ * Method: basic string manipulations by 
+ * building a new array.
  */
 void strinsert(char *str)
 {
@@ -37,10 +37,52 @@ void strinsert(char *str)
     strncpy(str, buf, BUFSIZ);
 }
 
+/**
+ * Method: change the string in place.
+ */
+void replace_string(char *str)
+{
+    // nlen = length of the extended string
+    int len, nlen; 
+    int space_num = 0;
+    bool endstr = false;
+    
+    len = strlen(str);
+    /* 
+     * scan # of spaces (drop redundant spaces
+     * at the end of the string before that).
+     */
+    for (int i = 0; i < len; i++)
+        if ( str[i] == ' ' )
+            for (int j = i + 1; str[j] == ' '; j++)
+                if ( j == len - 1) {
+                    len = i + 1;
+                    break;
+                }
+    str[len - 1] = '\0';        
+    for (int i = 0; i < len; i++) 
+        if ( str[i] == ' ' ) 
+                space_num++;
+
+    /*
+     * now move the string and insert new strings.
+     */
+    nlen = len + space_num * 2;
+    for (int i = len - 1, j = nlen - 1; i >= 0; i--) {
+        if ( str[i] != ' ' ) 
+            str[j--] = str[i];
+        else {
+            str[j--] = '0';
+            str[j--] = '2';
+            str[j--] = '%';
+        }
+    }
+}
 
 int main()
 {
-    char str[BUFSIZ] = "Hello my dear!!!";
-    strinsert_v1(str);
-    cout << str1 << endl;
+    char str[] = "Mr John Smith    ";
+    //strinsert(str);
+    replace_string(str);
+    cout << str << endl;
 } 
