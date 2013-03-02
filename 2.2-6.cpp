b80de00e0ca0e9e31422df7393ca028d6ff33977
moophis <moophis@github.com>
2013-03-02 10:43:41 +0800

    careercup  2.2 - 2.6

--- 2.2-6.cpp
+++ 2.2-6.cpp
@@ -259,48 +259,48 @@ void list_partition(int x, node* &head, node* &tail)
   */
 void reverse_sum(node* &head1, node* &tail1, node* &head2, node* &tail2)
 {
-	node *current1 = head1->next, *current2 = head2->next;
-	node *sum_head, *sum_tail;
-	int carry = 0, thisbit = 0;
-	
-	init_list(sum_head, sum_tail);
-	
-	// first, deal with the common length parts of two lists
-	while ( current1 != tail1 && current2 != tail2 ) {
-		thisbit = current1->data + current2->data + carry;
-		carry = thisbit / 10;
-		thisbit = thisbit % 10;
-		add_node(thisbit, sum_head, sum_tail);
-		
-		current1 = current1->next;
-		current2 = current2->next;
-	}
-	
-	// then deal with extra length (if has) of one list
-	if ( current1 == tail1 ) {
-		while ( current2 != tail2 ) {
-			thisbit = current2->data + carry;
-			carry = thisbit / 10;
-			thisbit = thisbit % 10;
-			add_node(thisbit, sum_head, sum_tail);
-			
-			current2 = current2->next;
-		}
-	} else if ( current2 == tail2 ) {
-		while ( current1 != tail1 ) {
-			thisbit = current1->data + carry;
-			carry = thisbit / 10;
-			thisbit = thisbit % 10;
-			add_node(thisbit, sum_head, sum_tail);
-			
-			current1 = current1->next;
-		}
-	}
-	if ( carry != 0 ) 
-		add_node(carry, sum_head, sum_tail);
-		
-	// show results
-	show_list(sum_head, sum_tail);
+    node *current1 = head1->next, *current2 = head2->next;
+    node *sum_head, *sum_tail;
+    int carry = 0, thisbit = 0;
+    
+    init_list(sum_head, sum_tail);
+    
+    // first, deal with the common length parts of two lists
+    while ( current1 != tail1 && current2 != tail2 ) {
+        thisbit = current1->data + current2->data + carry;
+        carry = thisbit / 10;
+        thisbit = thisbit % 10;
+        add_node(thisbit, sum_head, sum_tail);
+        
+        current1 = current1->next;
+        current2 = current2->next;
+    }
+    
+    // then deal with extra length (if has) of one list
+    if ( current1 == tail1 ) {
+        while ( current2 != tail2 ) {
+            thisbit = current2->data + carry;
+            carry = thisbit / 10;
+            thisbit = thisbit % 10;
+            add_node(thisbit, sum_head, sum_tail);
+            
+            current2 = current2->next;
+        }
+    } else if ( current2 == tail2 ) {
+        while ( current1 != tail1 ) {
+            thisbit = current1->data + carry;
+            carry = thisbit / 10;
+            thisbit = thisbit % 10;
+            add_node(thisbit, sum_head, sum_tail);
+            
+            current1 = current1->next;
+        }
+    }
+    if ( carry != 0 ) 
+        add_node(carry, sum_head, sum_tail);
+        
+    // show results
+    show_list(sum_head, sum_tail);
 }
 
 /**
@@ -309,62 +309,107 @@ void reverse_sum(node* &head1, node* &tail1, node* &head2, node* &tail2)
  */
 void forward_sum(node* &head1, node* &tail1, node* &head2, node* &tail2)
 {
-	stack<int> s1, s2, sum;
-	node *current1 = head1->next, *current2 = head2->next;
-	node *sum_head, *sum_tail;
-	int carry = 0, thisbit = 0;
-	
-	init_list(sum_head, sum_tail);
+    stack<int> s1, s2, sum;
+    node *current1 = head1->next, *current2 = head2->next;
+    node *sum_head, *sum_tail;
+    int carry = 0, thisbit = 0;
+    
+    init_list(sum_head, sum_tail);
 
-	for ( ;current1 != tail1; current1 = current1->next) 
-		s1.push(current1->data);
-	for ( ;current2 != tail2; current2 = current2->next)
-		s2.push(current2->data);
+    for ( ;current1 != tail1; current1 = current1->next) 
+        s1.push(current1->data);
+    for ( ;current2 != tail2; current2 = current2->next)
+        s2.push(current2->data);
 
-	while ( !s1.empty() && !s2.empty() ) {
-		thisbit = s1.top() + s2.top() + carry;
-		carry = thisbit / 10;
-		thisbit = thisbit % 10;
-		add_node(thisbit, sum_head, sum_tail);
-		
-		s1.pop(); 
-		s2.pop();
-	}
-	
-	if ( s1.empty() ) {
-		while ( !s2.empty() ) {
-			thisbit = s2.top() + carry;
-			carry = thisbit / 10;
-			thisbit = thisbit % 10;
-			sum.push(thisbit);
-			
-			s2.pop();
-		}
-	} else if ( s2.empty() ) {
-		while ( !s1.empty() ) {
-			thisbit = s1.top() + carry;
-			carry = thisbit / 10;
-			thisbit = thisbit % 10;
-			sum.push(thisbit);
-			
-			s1.pop();
-		}
-	}
-	
-	if ( carry != 0 ) 
-		sum.push(carry);
-		
-	while ( !sum.empty() ) {
-		add_node(sum.top(), sum_head, sum_tail);
-		sum.pop();
-	}
-	show_list(sum_head, sum_tail);
+    while ( !s1.empty() && !s2.empty() ) {
+        thisbit = s1.top() + s2.top() + carry;
+        carry = thisbit / 10;
+        thisbit = thisbit % 10;
+        add_node(thisbit, sum_head, sum_tail);
+        
+        s1.pop(); 
+        s2.pop();
+    }
+    
+    if ( s1.empty() ) {
+        while ( !s2.empty() ) {
+            thisbit = s2.top() + carry;
+            carry = thisbit / 10;
+            thisbit = thisbit % 10;
+            sum.push(thisbit);
+            
+            s2.pop();
+        }
+    } else if ( s2.empty() ) {
+        while ( !s1.empty() ) {
+            thisbit = s1.top() + carry;
+            carry = thisbit / 10;
+            thisbit = thisbit % 10;
+            sum.push(thisbit);
+            
+            s1.pop();
+        }
+    }
+    
+    if ( carry != 0 ) 
+        sum.push(carry);
+        
+    while ( !sum.empty() ) {
+        add_node(sum.top(), sum_head, sum_tail);
+        sum.pop();
+    }
+    show_list(sum_head, sum_tail);
+}
+
+/** 
+ * careercup 2.6
+ * Given a circular linked list, implement an algorithm which 
+ * returns the node at the beginning of the loop.
+ * DEFINITION
+ * Circular linked list: A (corrupt) linked list in which 
+ * a node's next pointer points to an earlier node, so as to 
+ * make a loop in the linked list.
+ * EXAMPLE
+ * Input: A -> B -> C -> D -> E -> C [the same C as earlier]
+ * Output: C
+ * 
+ * Method: Use two pointers. One pointer moves 1 slot per iteration,
+ * another 2 slots. If there is a loop in the linked list, they must
+ * meet somewhere. When meet, move one pointer back to head, then 
+ * move two pointers 1 slot per iteration at the same time. When they 
+ * meet again, the node in which they reside is the beginning of the loop.
+ */
+node* detect_circle(node* &head, node* &tail)
+{
+    node *slow, *fast;
+    bool has_met_once = false; // when two pointers first met, set it true.
+    
+    slow = fast = head;
+    if ( head->next == tail || head->next == head )
+        return NULL;
+        
+    while ( fast != tail && fast->next != tail ) {
+        slow = slow->next;
+        if ( !has_met_once ) {
+            fast = fast->next->next;
+            if ( slow == fast ) {
+                slow = head;
+                has_met_once = true;
+            }
+        } else {
+            fast = fast->next;
+            if ( slow == fast ) // meet again
+                return slow;
+        }
+    }
+    // loop detected
+    return NULL;
 }
 
 int main()
 {
     int arr[5] = {5, 4, 3, 2, 5};
-    node *head, *tail;
+    node *head, *tail, *current;
     
     array_to_list(arr, 5, head, tail);
     //show_list(head, tail);
@@ -382,17 +427,31 @@ int main()
     cout << "----2.4 results----" << endl;
     list_partition(5, head, tail);
     show_list(head, tail);
+
+    // for 2.5
+    cout << "----2.5 results----" << endl;
+    int fst[5] = {5, 9, 3, 9, 9};
+    int sec[3] = {7, 1, 6};
+    node *head1, *head2, *tail1, *tail2;
+    
+    array_to_list(fst, 5, head1, tail1);
+    array_to_list(sec, 3, head2, tail2);
+    reverse_sum(head1, tail1, head2, tail2);
+    forward_sum(head1, tail1, head2, tail2);
 #endif
-	// for 2.5
-	cout << "----2.5 results----" << endl;
-	int fst[5] = {5, 9, 3, 9, 9};
-	int sec[3] = {7, 1, 6};
-	node *head1, *head2, *tail1, *tail2;
-	
-	array_to_list(fst, 5, head1, tail1);
-	array_to_list(sec, 3, head2, tail2);
-	reverse_sum(head1, tail1, head2, tail2);
-	forward_sum(head1, tail1, head2, tail2);
-	
+    // for 2.6
+    cout << "----2.6 results----" << endl;
+    // make a loop
+    node *result;
+    current = head;
+    while ( current->next != tail ) 
+        current = current->next;
+    current->next = head->next->next;
+    
+    if ( !(result = detect_circle(head, tail)) )
+        cout << "No loop detected!" << endl;
+    else
+        cout << "The entry of the loop: " << result->data << endl;
+   
     return 0;
 }
