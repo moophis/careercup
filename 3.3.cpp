74e988d54ba003e06b26602d0c3f7b1af082c3c1
moophis <moophis@github.com>
2013-03-05 21:08:58 +0800

    careercup 3.3

--- 3.3.cpp
+++ 3.3.cpp
@@ -29,8 +29,8 @@ using namespace std;
 template <class T>
 class set_of_stacks {
 private:
-    list<T> *ptr_storage;   // store stacks
-    int ptr_size;           // # of stacks
+    list<T> *stk_storage;   // store stacks
+    int stk_size;           // # of stacks
     int list_size;          // capacity of each stack
     int current_ptr;        // store in which stack top pointer resides
     int empty_stack_num;    // # of empty stack
@@ -39,14 +39,14 @@ public:
      * constructor
      */
     set_of_stacks(int init_ptr_num, int max_list_size) 
-        : ptr_size(init_ptr_num), list_size(max_list_size) {
-        assert(ptr_size >= 1 && list_size >= 1);
+        : stk_size(init_ptr_num), list_size(max_list_size) {
+        assert(stk_size >= 1 && list_size >= 1);
         cout << "In constructor..." << endl;
         // build pointers table
-        ptr_storage = new list<T> [ptr_size];
+        stk_storage = new list<T> [stk_size];
         // assemble the first linked list as a initialized stack
         current_ptr = 0;
-        empty_stack_num = ptr_size;
+        empty_stack_num = stk_size;
     }
     
     /* 
@@ -55,7 +55,7 @@ public:
     ~set_of_stacks() {
         assert(current_ptr >= 0);
         cout << "In destructor..." << endl;
-        delete[] ptr_storage;
+        delete[] stk_storage;
     }
     
     /* 
@@ -63,24 +63,24 @@ public:
      */
     void push(T val) {
         // check whether we need to push element in a new stack
-        if ( ptr_storage[current_ptr].size() == list_size )
+        if ( stk_storage[current_ptr].size() == list_size )
             current_ptr++;
         // check whether the pointer table is full 
-        if ( current_ptr == ptr_size ) {
+        if ( current_ptr == stk_size ) {
             // if so, double size the table
-            list<T> *tmp_storage = new list<T> [ptr_size * 2];
+            list<T> *tmp_storage = new list<T> [stk_size * 2];
             
-            empty_stack_num += ptr_size;
-            for (int i = 0; i < ptr_size; i++)
-                tmp_storage[i] = ptr_storage[i];
-            delete[] ptr_storage;
-            ptr_storage = tmp_storage;
-            ptr_size += ptr_size;
+            empty_stack_num += stk_size;
+            for (int i = 0; i < stk_size; i++)
+                tmp_storage[i] = stk_storage[i];
+            delete[] stk_storage;
+            stk_storage = tmp_storage;
+            stk_size += stk_size;
         }
         // now we can push element as usual
-        if ( ptr_storage[current_ptr].size() == 0 )
+        if ( stk_storage[current_ptr].size() == 0 )
             empty_stack_num--;
-        ptr_storage[current_ptr].push_back(val);
+        stk_storage[current_ptr].push_back(val);
     }
     
     /* 
@@ -90,31 +90,31 @@ public:
         T retval;
         // check whether current stack is empty
         // be careful about the impact pop_at() may bring.
-        while ( ptr_storage[current_ptr].size() == 0 ) {
+        while ( stk_storage[current_ptr].size() == 0 ) {
             assert(current_ptr > 0);
             current_ptr--;
         } 
-        retval = ptr_storage[current_ptr].back();
-        ptr_storage[current_ptr].pop_back();
+        retval = stk_storage[current_ptr].back();
+        stk_storage[current_ptr].pop_back();
         
-        if ( ptr_storage[current_ptr].size() == 0 )
+        if ( stk_storage[current_ptr].size() == 0 )
             empty_stack_num++;
         return retval;
     }
     
     /*
      * method: pop_at
-     * index should be [0, ptr_size)
+     * index should be [0, stk_size)
      */
     T pop_at(int index) {
         T retval; 
         
-        assert(index >= 0 && index < ptr_size);
-        assert(ptr_storage[index].size() > 0);
-        retval = ptr_storage[index].back();
-        ptr_storage[index].pop_back();
+        assert(index >= 0 && index < stk_size);
+        assert(stk_storage[index].size() > 0);
+        retval = stk_storage[index].back();
+        stk_storage[index].pop_back();
         
-        if ( ptr_storage[index].size() == 0 )
+        if ( stk_storage[index].size() == 0 )
             empty_stack_num++;
         
         return retval;
@@ -124,23 +124,23 @@ public:
      * method: query whether certain stack is empty
      */
     bool empty(int index) {
-        assert(index >= 0 && index < ptr_size);
+        assert(index >= 0 && index < stk_size);
         
-        return ptr_storage[index].size() == 0;
+        return stk_storage[index].size() == 0;
     }
     
     /*
      * method: query whether stack set is empty
      */
     bool all_empty() {
-        return empty_stack_num == ptr_size;
+        return empty_stack_num == stk_size;
     }
     
     /*
      * method: get stack number
      */
     int stack_num() {
-        return ptr_size;
+        return stk_size;
     }
     
     /*
